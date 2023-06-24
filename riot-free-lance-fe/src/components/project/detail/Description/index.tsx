33afa58f8313d6e5babbebb2bf15268fae2f74b7
RyunosukeIshihara-124 <ryunosuke.ishihara0124@gmail.com>
2023-06-25 00:56:22 +0900

    fix

--- riot-free-lance-fe/src/components/project/detail/Description/index.tsx
+++ riot-free-lance-fe/src/components/project/detail/Description/index.tsx
@@ -15,49 +15,49 @@ export const Description = ({ projectDetailData }: Props) => {
       {detailDescription.businessContent && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>業務内容</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.businessContent}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.businessContent }} />
         </Styles.SecDescription>
       )}
       {detailDescription.developmentEnvironment && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>開発環境</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.developmentEnvironment}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.developmentEnvironment }} />
         </Styles.SecDescription>
       )}
       {detailDescription.requiredSkills && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>必須スキル</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.requiredSkills}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.requiredSkills }} />
         </Styles.SecDescription>
       )}
       {detailDescription.welcomeSkills && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>歓迎スキル</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.welcomeSkills}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.welcomeSkills }} />
         </Styles.SecDescription>
       )}
       {detailDescription.personnelSought && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>求める人材</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.personnelSought}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.personnelSought }} />
         </Styles.SecDescription>
       )}
       {detailDescription.workingConditions && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>稼働条件</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.workingConditions}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.workingConditions }} />
         </Styles.SecDescription>
       )}
       {detailDescription.recruitmentBackground && (
         <Styles.SecDescription>
           <Styles.HeadInfoTitle>募集背景</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.recruitmentBackground}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.recruitmentBackground }} />
         </Styles.SecDescription>
       )}
       {detailDescription.contentBusiness && (
         <Styles.SecLastDescription>
           <Styles.HeadInfoTitle>事業内容</Styles.HeadInfoTitle>
-          <Styles.PerInfoDescription>{detailDescription.contentBusiness}</Styles.PerInfoDescription>
+          <Styles.PerInfoDescription dangerouslySetInnerHTML={{ __html: detailDescription.contentBusiness }} />
         </Styles.SecLastDescription>
       )}
     </Styles.DivDescriptionWrpper>
--- riot-free-lance-fe/src/lib/apollo/client.ts
+++ riot-free-lance-fe/src/lib/apollo/client.ts
@@ -1,28 +1,22 @@
-import {
-    ApolloClient,
-    HttpLink,
-    InMemoryCache,
-    NormalizedCacheObject,
-  } from "@apollo/client";
-  import "cross-fetch/polyfill";
-  
-  let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
-  const createApolloClient = () => {
-    return new ApolloClient({
-      ssrMode: typeof window === "undefined",
-      link: new HttpLink({
-        uri: "https://famous-walrus-45.hasura.app/v1/graphql",
-      }),
-      cache: new InMemoryCache(),
-    });
-  };
-  export const initializeApollo = () => {
-    const _apolloClient = apolloClient ?? createApolloClient();
-    // For SSG and SSR always create a new Apollo Client
-    if (typeof window === "undefined") return _apolloClient;
-    // Create the Apollo Client once in the client
-    if (!apolloClient) apolloClient = _apolloClient;
-  
-    return _apolloClient;
-  };
-  
+import { ApolloClient, HttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
+import "cross-fetch/polyfill";
+
+let apolloClient: ApolloClient<NormalizedCacheObject> | undefined;
+const createApolloClient = () => {
+  return new ApolloClient({
+    ssrMode: typeof window === "undefined",
+    link: new HttpLink({
+      uri: "https://famous-walrus-45.hasura.app/v1/graphql",
+    }),
+    cache: new InMemoryCache(),
+  });
+};
+export const initializeApollo = () => {
+  const _apolloClient = apolloClient ?? createApolloClient();
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === "undefined") return _apolloClient;
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient;
+
+  return _apolloClient;
+};
