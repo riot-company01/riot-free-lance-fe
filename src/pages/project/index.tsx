47731b0a5ed42615238df031f1ccb950bfca9c01
MurayamaKenta <imken924@gmail.com>
2023-06-26 00:09:58 +0900

    ログインしている場合に表示する

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -4,16 +4,26 @@ import { ProjectDetail } from "@/components/project/detail";
 import { ProjectList } from "@/components/project/list";
 import { PATHS } from "@/const/paths";
 import { useRouter } from "next/router";
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 
 export default function ProjectPage() {
   const query = useQueryString();
   const router = useRouter();
+  const { user } = useUser();
+
 
   return (
     <>
       <Header />
-      {router.asPath.includes("id") && <ProjectDetail projectId={Number(query?.id)} />}
-      {router.asPath === PATHS.PROJECT && <ProjectList />}
+      { user ? 
+        <>
+           {router.asPath.includes("id") && <ProjectDetail projectId={Number(query?.id)} />}
+           {router.asPath === PATHS.PROJECT && <ProjectList />}
+        </>
+       : 
+       <>ログインしてください。</> 
+      }
     </>
   );
 }
