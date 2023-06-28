afb5ee7aa008f1bf42d1ec3a14e95a8a50499d9b
ryunosuke-ishihara-RIOT <ryunosuke.ishihara8124@gmail.com>
2023-06-28 14:43:33 +0900

    fix

--- src/pages/api/auth/[...auth0].ts
+++ src/pages/api/auth/[...auth0].ts
@@ -1,10 +1,2 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
-
-export default handleAuth({
-  async login(req, res) {
-    // ログインの処理をここで行います
-    await handleLogin(req, res, {
-      returnTo: "/api/auth/callback", // ログイン成功後のリダイレクト先URLを指定します
-    });
-  },
-});
+import { handleAuth } from "@auth0/nextjs-auth0";
+export default handleAuth();
