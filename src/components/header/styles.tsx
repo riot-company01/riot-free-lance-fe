4d81085cd60eafe089cacb78aef89adea8efde2c
RyunosukeIshihara-124 <ryunosuke.ishihara0124@gmail.com>
2023-06-29 12:27:08 +0900

    fix

--- src/components/header/styles.tsx
+++ src/components/header/styles.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const HeaderContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
   background-color: #f5f5f5;
   padding: 20px;
