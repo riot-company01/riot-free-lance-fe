c9f5cf17867bd754e4907ad1c94d42a60af2b37f
shun shimono <68690488+shunshimono@users.noreply.github.com>
2023-07-10 23:36:02 +0900

    fix

--- src/components/common/layout/index.tsx
+++ src/components/common/layout/index.tsx
@@ -1,31 +1,51 @@
 import styled from "@emotion/styled";
 import Paper from "@mui/material/Paper";
 import InputBase from "@mui/material/InputBase";
-import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
+import Link from "@mui/material/Link";
+import NextLink from "next/link";
 import SearchIcon from "@mui/icons-material/Search";
-import DirectionsIcon from "@mui/icons-material/Directions";
 
 export const Layout: React.FC = () => {
   return (
     <Wrapper>
       <FlexContainer>
-        <List>
-          <Logo src="images/company-logo.png" alt="ライオット" />
-        </List>
+        <NextLink href="#">
+          <List>
+            <Logo src="images/company-logo.png" alt="ライオット" />
+          </List>
+        </NextLink>
+
         <List>
           <CustomizedInputBase></CustomizedInputBase>
         </List>
-        <List>検索履歴</List>
-        <List>条件</List>
+
+        <NextLink href="#">
+          <List>検索履歴</List>
+        </NextLink>
+        <NextLink href="#">
+          <List>条件</List>
+        </NextLink>
       </FlexContainer>
       <FlexContainer>
-        <List>案件を探す</List>
-        <List>エージェントを探す</List>
-        <List>スカウト</List>
-        <List>情報</List>
-        <List>ログイン</List>
-        <List>新規登録</List>
+        <NextLink href="#">
+          <List>案件を探す</List>
+        </NextLink>
+        <NextLink href="#">
+          <List>エージェントを探す</List>
+        </NextLink>
+        <NextLink href="#">
+          <List>スカウト</List>
+        </NextLink>
+        <NextLink href="#">
+          <List>情報</List>
+        </NextLink>
+        <NextLink href="#">
+          <List>ログイン</List>
+        </NextLink>
+        <NextLink href="#">
+          <LastList>新規登録</LastList>
+        </NextLink>
       </FlexContainer>
     </Wrapper>
   );
@@ -43,9 +63,11 @@ const Logo = styled.img`
 const List = styled.div`
   font-size: 14px;
   padding-left: 20px;
-  :last-of-type {
-    padding-right: 20px;
-  }
+`;
+const LastList = styled.div`
+  font-size: 14px;
+  padding-left: 20px;
+  padding-right: 20px;
 `;
 
 const Wrapper = styled.div`
