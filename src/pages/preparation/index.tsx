9779d94765ac3c1109d8288cdc4e22eabc6941b1
RyunosukeIshihara-124 <ryunosuke.ishihara0124@gmail.com>
2023-07-31 21:19:30 +0900

    fix

--- src/pages/preparation/index.tsx
+++ src/pages/preparation/index.tsx
@@ -14,7 +14,7 @@ export default function Preparation() {
   return (
     <CenteredContainer>
       <Container>
-        <Message>ただいまこちらのページはです</Message>
+        <Message>ただいまこちらのページは準備中です</Message>
         <Button variant="contained" onClick={transitWorksPage}>
           案件ページに戻る
         </Button>
