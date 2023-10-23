1fdc0038ec6decadf59721cbfa8677c69b3b9f72
shun shimono <68690488+shunshimono@users.noreply.github.com>
2023-10-23 20:22:43 +0900

    fix

--- src/pages/works/index.tsx
+++ src/pages/works/index.tsx
@@ -1,4 +1,4 @@
-import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { useUser } from "@auth0/nextjs-auth0/client";
 import { Box } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
@@ -7,28 +7,28 @@ import { WorksLg } from "@/components/works/lg";
 import { WorksMd } from "@/components/works/md";
 import { NotResult } from "@/components/works/shared/not-result";
 import { LG_BREAK_POINT, MD_BREAK_POINT } from "@/constants";
-import { initializeApollo, addApolloState } from "@/lib/apollo/client";
 import type { GetSkillsQuery } from "@/lib/graphql/graphql";
 
-export const getServerSideProps = withPageAuthRequired({
-  // @ts-ignore
-  async getServerSideProps() {
-    const client = initializeApollo({});
-    const documentProps = addApolloState(client, {
-      props: {},
-    });
-    return {
-      props: documentProps.props,
-    };
-  },
-});
+// export const getServerSideProps = withPageAuthRequired({
+//   // @ts-ignore
+//   async getServerSideProps() {
+//     const client = initializeApollo({});
+//     const documentProps = addApolloState(client, {
+//       props: {},
+//     });
+//     return {
+//       props: documentProps.props,
+//     };
+//   },
+// });
 
 function Works() {
   const { skillsData, worksData } = useApiRequest();
   const router = useRouter();
+  const data = useUser();
   const selectedSkillIds = (router.query["skill-ids"] as string | undefined)?.split(",") || [];
   const [skills, setSkills] = useState<GetSkillsQuery["skills"] | undefined>([]);
-
+  console.log(data);
   useEffect(() => {
     if (skillsData) {
       setSkills(skillsData?.skills);
