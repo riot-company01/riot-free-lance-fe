bfe6886c32b5b8079541c2c62ad60b600df7b507
RyunosukeIshihara-124 <ryunosuke.ishihara0124@gmail.com>
2023-10-26 13:19:30 +0900

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,29 +15,19 @@ function MyApp({ Component, pageProps }: AppProps) {
   const client = useCustomApollo(pageProps);
   const { user } = pageProps;
 
-  const redirect_uri = typeof window !== "undefined" ? `http://localhost:3000/works/api/auth/callback/` : undefined;
-
   return (
-    <Auth0Provider
-      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN || "https://dev-3obkayqmfl5rdnz2.jp.auth0.com"}
-      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || "EbnnOaqN4SUc3EYw0sembXxSJCA9cyD9"}
-      authorizationParams={{
-        redirect_uri,
-      }}
-    >
-      <UserProvider user={user}>
-        <StyledEngineProvider injectFirst>
-          <ApolloProvider client={client}>
-            <Global styles={GLOBAL_STYLE} />
-            <LayoutHeader />
-            <MaxWidth>
-              <Component {...pageProps} />
-            </MaxWidth>
-            <LayoutFooter />
-          </ApolloProvider>
-        </StyledEngineProvider>
-      </UserProvider>
-    </Auth0Provider>
+    <UserProvider user={user}>
+      <StyledEngineProvider injectFirst>
+        <ApolloProvider client={client}>
+          <Global styles={GLOBAL_STYLE} />
+          <LayoutHeader />
+          <MaxWidth>
+            <Component {...pageProps} />
+          </MaxWidth>
+          <LayoutFooter />
+        </ApolloProvider>
+      </StyledEngineProvider>
+    </UserProvider>
   );
 }
 
